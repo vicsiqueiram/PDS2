f3eec36710d2f4b86416ceda7ed63f3c420b1a5b
vicsiqueiram <112454628+vicsiqueiram@users.noreply.github.com>
2022-11-01 22:25:58 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "intruso.hpp"
+
+int main(){
+    int num_entradas, i;
+    std::string senha;
+    Intruso intruso;
+    
+    std::cin >> num_entradas;
+    std::cin.ignore();
+
+    for(i=0; i < num_entradas; i++){
+        std::getline(std::cin, senha);
+        intruso.set_senha_vazada(senha);
+    }
+    std::cout << intruso.crack_senha() <<std::endl;
+    
+    return 0;
+}
