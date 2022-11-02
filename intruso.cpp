9ae81532e6daa432d2983bc6a6157712787fc5ed
MarlonSoares <marlons.silva@hotmail.com>
2022-11-01 23:45:33 -0300

    Update intruso.cpp

--- intruso.cpp
+++ intruso.cpp
@@ -1,12 +1,12 @@
 #include "intruso.hpp"
-void Intruso::set_senha_vazada(std::string vazou){
+void Intruso::set_senha_vazada(std::string vazada){
     std::map<char, std::pair<char, char>> codigo;
     std::vector<char> senha;
     for(int i=0;i<5;i++){
-        codigo[char('A'+i)] = std::pair<char, char> (vazou[4*i],vazou[4*i+2]);
+        codigo[char('A'+i)] = std::pair<char, char> (vazada[4*i],vazada[4*i+2]);
     }
     for(int i=10;i<16;i++){
-        senha.push_back(vazou[2*i]);
+        senha.push_back(vazada[2*i]);
     }
     armazenamento.push_back(std::pair<std::map<char, std::pair<char, char>>, std::vector<char>>(codigo, senha));
 }
